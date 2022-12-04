2646a847100ea913c87444d15dc90dbba292b770
iigmir <roc120j@gmail.com>
2022-12-04 15:01:54 +0800

    static

--- index.js
+++ index.js
@@ -3,13 +3,16 @@ const app = express();
 const port = 3000;
 
 app.get("/api", (req, res) => {
+    res.statusCode = 401;
     res.jsonp({
         token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.oJDyH4x4NVDO-1yan7zMc_ET60Iq9iPT348Oq4QSGyE"
     });
 });
 
+app.use("/static", express.static("static"));
+
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+    res.redirect("/static");
 });
 
 app.listen(port, () => {
