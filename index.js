f8bde99be35939b93a5a06012437e7152a94737d
iigmir <roc120j@gmail.com>
2022-12-04 16:41:39 +0800

    Refactor

--- index.js
+++ index.js
@@ -4,38 +4,37 @@ import jwt_decode from "jwt-decode";
 const app = express();
 const port = 3000;
 
-// 流浪到淡水
-app.get("/api",  (req, res) => {
-    const authorization = (code) => {
+const api_cb = (req, res) => {
+    const get_decode = (code) => {
         try {
             return jwt_decode(code, { header: false });
         } catch (error) {
             return { signed: false, throwed: true, error };
         }
     };
-    const decoded = authorization(req.headers.authorization);
+    const decoded = get_decode(req.headers.authorization);
     const code = decoded.signed ? 200 : 401;
     const msg = decoded.signed ? "Authorised" : "Unauthorised";
-    if( decoded.throwed ) {
-        res.jsonp({ code: 400, error: decoded.error });
+    if (decoded.throwed) {
+        res.jsonp({ code: 500, error: decoded.error });
         return;
     }
     res.jsonp({ msg, code });
-});
+};
 
-app.get("/api/token", (req, res) => {
+const token_cb = (req, res) => {
     res.statusCode = 200;
     res.jsonp({
         token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwic2lnbmVkIjp0cnVlLCJpYXQiOjE1MTYyMzkwMjJ9.4bXcQktUlw1RopkiZvOKxq3WdNwnhrjABxWr09xdo4E",
     });
-});
+};
 
-app.use("/static", express.static("static"));
-
-app.get("/", (req, res) => {
+const use_static = (req, res) => {
     res.redirect("/static");
-});
+};
 
-app.listen(port, () => {
-    console.warn(`Example app listening: http://127.0.0.1:${port}`);
-});
+app.get("/api",  api_cb);
+app.get("/api/token", token_cb);
+app.use("/static", express.static("static"));
+app.get("/", use_static);
+app.listen(port, () => { console.log(`Example app listening: http://127.0.0.1:${port}`); });
--- static/index.js
+++ static/index.js
@@ -1,59 +1,35 @@
 const ajax = axios.create();
 
-/**
- * Interceptor
- * @param {*} response 
- * @see <https://stackoverflow.com/a/73257544>
- * @returns {Promise}
- */
-const ins_res = (response) => {
-    console.log(response);
-    debugger;
-    const unauth = response.response.status === 401;
-    if( unauth ) {
-        axios({
-            method: "GET",
-            url: "/api/token"
-        }).then( ({ data }) => {
-            console.log(data.token);
-        });
-    }
-    return response;
-};
-
-ajax.interceptors.request.use( (config) => {
+const set_token = (config) => {
     const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwic2lnbmVkIjpmYWxzZSwiaWF0IjoxNTE2MjM5MDIyfQ.D0yO072iGsw34M5pkEytQ_fsyEVZxQvL4xWSdCF4rhQ";
     if (token) {
         config.headers["Authorization"] = `Bearer ${token}`;
     }
     return config;
-}, error => Promise.reject(error) );
+};
 
-ajax.interceptors.response.use( function (response) {
-    // Any status code that lie within the range of 2xx cause this function to trigger
-    // Do something with response data
+const regain_token = function (response) {
     if (response.data.code === 401) {
-        return new Promise( (resolve, reject) => {
-            axios({
-                method: "GET",
-                url: "/api/token"
-            }).then( ({ data }) => {
+        return new Promise((resolve, reject) => {
+            const sent_new_ajax = ({ data }) => {
                 response.config.headers["Authorization"] = `Bearer ${data.token}`;
                 const new_ajax = axios.request(response.config);
-                new_ajax.then( r => resolve(r.data) ).catch( e => reject(e) );
-            }).catch( e => reject(e) );
+                new_ajax.then(r => resolve(r.data)).catch(e => reject(e));
+            };
+            axios({ method: "GET", url: "/api/token" }).then(sent_new_ajax).catch(e => reject(e));
         });
     } else if (response.data.code !== 200) {
         return Promise.reject(response.data);
     } else {
         return Promise.resolve(response.data);
     }
-}, error => Promise.reject(error) );
+};
 
-ajax({
-    method: "GET",
-    url: "/api"
-}).then( (r) => {
+ajax.interceptors.request.use( set_token, error => Promise.reject(error) );
+ajax.interceptors.response.use( regain_token, error => Promise.reject(error) );
+
+// Main AJAX
+ajax({ method: "GET", url: "/api" }).then( (r) => {
     console.log(r);
     document.getElementById("res").innerText = r.msg;
 }).catch( (e) => {
