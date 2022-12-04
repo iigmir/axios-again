ca269397878d24d4b8d076375ba6f32a96e46e8e
iigmir <roc120j@gmail.com>
2022-12-04 15:27:44 +0800

    Add Interceptor and Authorization

--- index.js
+++ index.js
@@ -2,9 +2,22 @@ import express from "express";
 const app = express();
 const port = 3000;
 
-app.get("/api", (req, res) => {
-    console.log(req.headers.authorization);
-    res.statusCode = 401;
+// 流浪到淡水
+app.get("/api",  (req, res) => {
+    if( req.headers.authorization ) {
+        res.statusCode = 200;
+        res.jsonp({
+            msg: "Authorised"
+        });
+    } else {
+        res.statusCode = 401;
+        res.jsonp({
+            msg: "Unauthorised"
+        });
+    }
+});
+
+app.get("/api/token", (req, res) => {
     res.jsonp({
         token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.oJDyH4x4NVDO-1yan7zMc_ET60Iq9iPT348Oq4QSGyE"
     });
--- static/index.js
+++ static/index.js
@@ -1,10 +1,21 @@
-// Interceptor
 const ajax = axios.create();
-ajax.interceptors.response.use( (response) => {
-    return response;
-}, (error) => {
+
+/**
+ * Interceptor
+ * @param {*} error 
+ * @see <https://stackoverflow.com/a/73257544>
+ * @returns {Promise}
+ */
+const ins = (error) => {
+    const unauth = error.response.status === 401;
+    if( unauth ) {
+        console.log(error.response.data.token);
+    }
     return Promise.reject(error);
-});
+};
+
+ajax.interceptors.response.use( (response) => response, ins);
+
 ajax({
     method: "GET",
     url: "/api"
